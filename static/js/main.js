6edea84519a70fffcf39fd48c0e0523d76388eaa
eforzano <eforzano@gmail.com>
2016-10-26 01:09:29 -0500

    Added embeded YouTube player on the site

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,28 @@
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//  This function creates an <iframe> (and YouTube player)
+//  after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
+
+//  The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
 $(function(){
   $('button').click(function(){
     var bandName = document.getElementById("mySearch").value;
@@ -27,4 +52,5 @@ $(function(){
     });
   });
 });
+
 
